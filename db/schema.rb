66374225c6b8d3e7982aa8a9152a9881f690a427
mohankapaganti <mohan.kapaganti@gmail.com>
2020-06-06 16:37:07 +0530

    Updated database migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_070725) do
+ActiveRecord::Schema.define(version: 2020_04_30_132235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2020_04_13_070725) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_reset_token"
+    t.string "confirmation_token"
   end
 
 end
